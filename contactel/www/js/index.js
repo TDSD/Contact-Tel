8834f0159ee06351633eec8278d3ce38d1318f44
TDSD <69878261+TDSD@users.noreply.github.com>
2022-11-23 09:56:10 +0000

    Update index.js

--- contactel/www/js/index.js
+++ contactel/www/js/index.js
@@ -57,3 +57,89 @@ function addContact(){
     xhr.send(JSON.stringify(json));
     checkPage();  
 }
+
+function showContact(telephone){
+    const identifiant = {
+        'telephone': telephone
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        console.log(telephone);
+        let contact="test";
+        contact = JSON.parse(xhr.responseText);
+         console.log(contact); 
+                taskList.innerHTML = '';  
+                taskList.innerHTML += `
+                <li><img src="./img/avatar-n.png" style="width: 70px; height: 70px;" alt="avatar-contact"></li>
+                <li><span style="font-weight: bold;">Prénom :</span> ${contact.prenom}</li>
+                <li><span style="font-weight: bold;">Nom :</span> ${contact.nom}</li>
+                <li><span style="font-weight: bold;">Téléphone:</span> ${contact.telephone}</li>
+                <li><span style="font-weight: bold;">Email :</span> ${contact.email}</li>
+                <li><span style="font-weight: bold;">Adresse :</span> ${contact.adresse}</li> </br>
+                <a href="#updatePage"><input type="button" value="Modifier" onclick="updateBtnContact(${telephone})"></a>
+                <input type="button" value="Supprimer" onclick="deleteContact(${telephone})" style="margin-left: 20px;"> 
+            `;
+            $(taskList).listview('refresh');
+            
+    }
+    xhr.open("POST", "http://localhost:80/showContact.php");
+    xhr.send(JSON.stringify(identifiant));
+}
+
+function deleteContact(telephone){
+    const identifiant = {
+        'telephone': telephone
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        console.log(telephone);
+        contact = JSON.parse(xhr.responseText);    
+    }
+    xhr.open("POST", "http://localhost:80/deleteContact.php");
+    xhr.send(JSON.stringify(identifiant));
+    checkPage(); 
+}
+
+function updateBtnContact(telephone){
+    const identifiant = {
+        'telephone': telephone
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        console.log(telephone);
+        contact = JSON.parse(xhr.responseText);
+        formUpdate.innerHTML = '';  
+        formUpdate.innerHTML += `
+            <input type="text" id="prenom" placeholder="Prénom" value=" ${contact.prenom}"></br></br>
+            <input type="text" id="nom" placeholder="Nom" value="${contact.nom}"></br></br>
+            <input type="tel" id="telephone" placeholder="Téléphone" value="${contact.telephone}"></br></br>
+            <input type="email" id="email" placeholder="Email" value="${contact.email}"></br></br>
+            <input type="text" id="adresse" placeholder="Adresse" value="${contact.adresse}"></br></br>
+            <input type="button" value="Modifier" data-icon="action" data-iconpos="right" onclick="updateContact()">
+            `;    
+    }
+    xhr.open("POST", "http://localhost:80/showContact.php");
+    xhr.send(JSON.stringify(identifiant));
+}
+
+function updateContact(telephone){
+    const identifiant = {
+        'telephone': telephone
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        console.log(telephone);
+        contact = JSON.parse(xhr.responseText);
+        formUpdate.innerHTML = '';  
+        formUpdate.innerHTML += `
+            <input type="text" id="prenom" placeholder="Prénom" value=" ${contact.prenom}">
+            <input type="text" id="nom" placeholder="Nom" value="${contact.nom}">
+            <input type="tel" id="telephone" placeholder="Téléphone" value="${contact.telephone}">
+            <input type="email" id="email" placeholder="Email" value="${contact.email}">
+            <input type="text" id="adresse" placeholder="Adresse" value="${contact.adresse}">
+            <input type="button" value="Modifier" data-icon="action" data-iconpos="right" onclick="updateContact()">
+            `;    
+    }
+    xhr.open("POST", "http://localhost:80/showContact.php");
+    xhr.send(JSON.stringify(identifiant));
+}
